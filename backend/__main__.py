91a47dced314549a9d25c71d5114bc3c578c26ed
alekseybeloded <belod-aleksej@yandex.ru>
2023-08-30 19:03:45 +0300

    feat: add error handlers
    1) error handler for ValidationError
    2) error handler for AppError

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,11 +1,25 @@
+from backend.teams.errors import AppError
 import logging
 
-from flask import Flask
 
-from backend.teams import app
+from backend.teams.views import app
+from pydantic import ValidationError
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+def handle_app_error(e: AppError) -> dict[str, str]:
+    return {'error': str(e)}, e.code
+
+
+def handle_validation_error(e: ValidationError) -> dict[str, str]:
+    return {'error': str(e)}, 422
+
+
+app.register_error_handler(AppError, handle_app_error)
+app.register_error_handler(ValidationError, handle_validation_error)
+
 
 def main():
     logger.info('hello world')
