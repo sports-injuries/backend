6c36559c71d5af3b27c9f50e2f3b08510fc445e7
alekseybeloded <belod-aleksej@yandex.ru>
2023-08-30 19:07:28 +0300

    feat: add errors module in teams package

---
+++ backend/teams/errors.py
@@ -0,0 +1,13 @@
+class AppError(Exception):
+    code = 500
+    def __init__(self, reason: str) -> None:
+        super().__init__(reason)
+        self.reason = reason
+
+class NotFoundError(AppError):
+    code = 404
+    def __init__(self, resource: str, uid: int) -> None:
+        super().__init__(f'{resource} [{uid}] not found')
+        self.name = resource
+        self.uid = uid
+
