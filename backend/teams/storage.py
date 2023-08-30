f33ad4d3321924c69a3c36174162423785ac2048
alekseybeloded <belod-aleksej@yandex.ru>
2023-08-30 20:33:43 +0300

    fix: join db with add method

--- backend/teams/storage.py
+++ backend/teams/storage.py
@@ -1,31 +1,41 @@
-from backend.teams.schemas import Team
+from backend.teams.schemas import Team as TeamSchemas
 from backend.teams.errors import NotFoundError
+from models import Team
+from db import db_session
 
 
 class Storage:
     def __init__(self):
-        self.teams: dict[int, Team] = {}
+        self.teams: dict[int, TeamSchemas] = {}
         self.last_uid = 0
 
 
-    def add(self, team: Team) -> Team:
-        self.last_uid += 1
-        team.uid = self.last_uid
-        self.teams[self.last_uid] = team
+    def add(self, team: TeamSchemas) -> TeamSchemas:
+        self.last_uid +=1
+        team = Team(name=team.name, description=team.description)
+        db_session.add(team)
+        db_session.commit()
+        team = Team.query.filter(Team.uid==self.last_uid)
+        team = TeamSchemas(uid=team.uid, name=team.name, description=team.description)
         return team
 
+        # self.last_uid += 1
+        # team.uid = self.last_uid
+        # self.teams[self.last_uid] = team
+        # return team
 
-    def get_all(self) -> list[Team]:
+
+    def get_all(self) -> list[TeamSchemas]:
         return list(self.teams.values())
 
 
-    def get_by_id(self, uid: int) -> Team:
+    def get_by_id(self, uid: int) -> TeamSchemas:
         if uid not in self.teams:
             raise NotFoundError('team', uid)
         return self.teams[uid]
 
 
-    def update(self, team: Team, uid: int) -> Team:
+    def update(self, team: TeamSchemas, uid: int) -> TeamSchemas:
         if uid not in self.teams:
             raise NotFoundError('team', uid)
 
