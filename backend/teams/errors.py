30aa5387fb51f6effd10372189cecb8d37d6a840
alekseybeloded <belod-aleksej@yandex.ru>
2023-08-31 12:42:49 +0300

    fix: fix style errors in errors.py

--- backend/teams/errors.py
+++ backend/teams/errors.py
@@ -1,11 +1,14 @@
 class AppError(Exception):
     code = 500
+
     def __init__(self, reason: str) -> None:
         super().__init__(reason)
         self.reason = reason
 
+
 class NotFoundError(AppError):
     code = 404
+
     def __init__(self, resource: str, uid: int) -> None:
         super().__init__(f'{resource} [{uid}] not found')
         self.name = resource
