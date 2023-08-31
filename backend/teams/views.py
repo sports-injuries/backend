51a30848a1f51eb52b1e06290a109ca179a8dfca
alekseybeloded <belod-aleksej@yandex.ru>
2023-08-31 11:45:34 +0300

    fix: fix mypy errors
    add return value for  def add,
    add check for payload in def add and def update

--- backend/teams/views.py
+++ backend/teams/views.py
@@ -3,6 +3,7 @@ from pydantic import ValidationError
 from backend.teams.schemas import Team
 from backend.teams.storage import Storage
 from backend.teams.errors import AppError
+from typing import Any
 
 
 app = Flask(__name__)
@@ -12,8 +13,12 @@ storage = Storage()
 
 
 @app.post('/api/teams/')
-def add():
+def add() -> tuple[dict[str, Any], int]:
     payload = request.json
+    if not payload:
+        raise AppError('empty payload')
+
+    payload['uid'] = -1
     team = Team(**payload)
     team =  storage.add(team)
     return team.dict(), 201
@@ -34,6 +39,9 @@ def get_by_id(uid):
 @app.put('/api/teams/<int:uid>')
 def update(uid):
     payload = request.json
+    if not payload:
+        raise AppError('empty payload')
+
     team = Team(**payload)
     team = storage.update(team, uid)
     return team.dict(), 200
