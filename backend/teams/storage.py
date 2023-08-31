5484809ddf362d48cda639d086167fd186e8dd24
alekseybeloded <belod-aleksej@yandex.ru>
2023-08-31 12:43:33 +0300

    fix: fix style errors in storage.py

--- backend/teams/storage.py
+++ backend/teams/storage.py
@@ -1,7 +1,7 @@
-from backend.teams.schemas import Team as TeamSchemas
 from backend.teams.errors import NotFoundError
-from models import Team
+from backend.teams.schemas import Team as TeamSchemas
 from db import db_session
+from models import Team
 
 
 class Storage:
@@ -9,13 +9,12 @@ class Storage:
         self.teams: dict[int, TeamSchemas] = {}
         self.last_uid = 0
 
-
     def add(self, team: TeamSchemas) -> TeamSchemas:
-        self.last_uid +=1
+        self.last_uid += 1
         team = Team(name=team.name, description=team.description)
         db_session.add(team)
         db_session.commit()
-        team = Team.query.filter(Team.uid==self.last_uid)
+        team = Team.query.filter(Team.uid == self.last_uid)
         team = TeamSchemas(uid=team.uid, name=team.name, description=team.description)
         return team
 
@@ -24,16 +23,13 @@ class Storage:
         # self.teams[self.last_uid] = team
         # return team
 
-
     def get_all(self) -> list[TeamSchemas]:
         return list(self.teams.values())
-
 
     def get_by_id(self, uid: int) -> TeamSchemas:
         if uid not in self.teams:
             raise NotFoundError('team', uid)
         return self.teams[uid]
-
 
     def update(self, team: TeamSchemas, uid: int) -> TeamSchemas:
         if uid not in self.teams:
