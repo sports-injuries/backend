25a654229b27a86770b7d0ae10653186994359e4
alekseybeloded <belod-aleksej@yandex.ru>
2023-09-01 12:38:42 +0300

    fix: connect storage methods with db
    fix methods: add, get_all, get_by_id, update, delete

--- backend/teams/storage.py
+++ backend/teams/storage.py
@@ -1,44 +1,49 @@
+from backend.db import db_session
+from backend.models import Team
 from backend.teams.errors import NotFoundError
-from backend.teams.schemas import Team as TeamSchemas
-from db import db_session
-from models import Team
+from backend.teams.schema import TeamSchema
 
 
 class Storage:
-    def __init__(self) -> None:
-        self.teams: dict[int, TeamSchemas] = {}
-        self.last_uid = 0
-
-    def add(self, team: TeamSchemas) -> TeamSchemas:
-        self.last_uid += 1
-        team = Team(name=team.name, description=team.description)
-        db_session.add(team)
+    def add(self, team: TeamSchema) -> TeamSchema:
+        entity = Team(name=team.name, description=team.description)
+        db_session.add(entity)
         db_session.commit()
-        team = Team.query.filter(Team.uid == self.last_uid)
-        team = TeamSchemas(uid=team.uid, name=team.name, description=team.description)
-        return team
+        return TeamSchema(uid=entity.uid, name=entity.name, description=entity.description)
 
-        # self.last_uid += 1
-        # team.uid = self.last_uid
-        # self.teams[self.last_uid] = team
-        # return team
+    def get_all(self) -> list[TeamSchema]:
+        entities = Team.query.all()
+        return [
+            TeamSchema(uid=entity.uid, name=entity.name, description=entity.description)
+            for entity in entities
+        ]
 
-    def get_all(self) -> list[TeamSchemas]:
-        return list(self.teams.values())
+    def get_by_id(self, uid: int) -> TeamSchema:
+        entity = Team.query.get(uid)
 
-    def get_by_id(self, uid: int) -> TeamSchemas:
-        if uid not in self.teams:
-            raise NotFoundError('team', uid)
-        return self.teams[uid]
-
-    def update(self, team: TeamSchemas, uid: int) -> TeamSchemas:
-        if uid not in self.teams:
+        if not entity:
             raise NotFoundError('team', uid)
 
-        self.teams[uid] = team
-        return team
+        return TeamSchema(uid=entity.uid, name=entity.name, description=entity.description)
+
+    def update(self, team: TeamSchema, uid: int) -> TeamSchema:
+        entity = Team.query.get(uid)
+
+        if not entity:
+            raise NotFoundError('team', uid)
+
+        entity.name = team.name
+        entity.description = team.description
+
+        db_session.commit()
+
+        return TeamSchema(uid=entity.uid, name=entity.name, description=entity.description)
 
     def delete(self, uid: int) -> None:
-        if uid not in self.teams:
+        entity = Team.query.get(uid)
+
+        if not entity:
             raise NotFoundError('team', uid)
-        self.teams.pop(uid)
+
+        db_session.delete(entity)
+        db_session.commit()
