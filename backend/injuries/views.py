951137769b8c222203ab7cf06577b7b7ccd06d19
alekseybeloded <belod-aleksej@yandex.ru>
2023-09-01 19:51:31 +0300

    feat: add views for resource 'injuries'
    add handlers: add, get_all, get_by_id, update, delete

---
+++ backend/injuries/views.py
@@ -0,0 +1,56 @@
+from typing import Any
+
+from flask import Blueprint, request
+
+from backend.errors import AppError
+from backend.injuries.schema import InjurySchema
+from backend.injuries.storage import Storage
+
+injury_view = Blueprint('injuries', __name__)
+
+storage = Storage()
+
+
+@injury_view.post('/')
+def add() -> tuple[dict[str, Any], int]:
+    payload = request.json
+
+    if not payload:
+        raise AppError('empty payload')
+
+    payload['uid'] = -1
+    injury = InjurySchema(**payload)
+    injury = storage.add(injury)
+
+    return injury.dict(), 201
+
+
+@injury_view.get('/')
+def get_all() -> tuple[list[dict[str, Any]], int]:
+    injuries = storage.get_all()
+    return [injury.dict() for injury in injuries], 200
+
+
+@injury_view.get('/<int:uid>')
+def get_by_id(uid: int) -> tuple[dict[str, Any], int]:
+    injury = storage.get_by_id(uid)
+    return injury.dict(), 200
+
+
+@injury_view.put('/<int:uid>')
+def update(uid: int) -> tuple[dict[str, Any], int]:
+    payload = request.json
+
+    if not payload:
+        raise AppError('empty payload')
+
+    injury = InjurySchema(**payload)
+    injury = storage.update(injury, uid)
+
+    return injury.dict(), 200
+
+
+@injury_view.delete('/<int:uid>')  # type: ignore
+def delete(uid: int) -> tuple[dict[None, None], int]:
+    storage.delete(uid)
+    return {}, 204
