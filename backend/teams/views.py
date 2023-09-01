87b7659a702a6082c5f49b33e93b33100cdfe540
alekseybeloded <belod-aleksej@yandex.ru>
2023-09-01 12:39:32 +0300

    fix: add tip annotations

--- backend/teams/views.py
+++ backend/teams/views.py
@@ -3,46 +3,47 @@ from typing import Any
 from flask import Blueprint, request
 
 from backend.teams.errors import AppError
-from backend.teams.schemas import Team
+from backend.teams.schema import TeamSchema
 from backend.teams.storage import Storage
 
 team_view = Blueprint('teams', __name__)
 
+Team = dict[str, Any]
 
 storage = Storage()
 
 
 @team_view.post('/')
-def add() -> tuple[dict[str, Any], int]:
+def add() -> tuple[Team, int]:
     payload = request.json
     if not payload:
         raise AppError('empty payload')
 
     payload['uid'] = -1
-    team = Team(**payload)
+    team = TeamSchema(**payload)
     team = storage.add(team)
     return team.dict(), 201
 
 
 @team_view.get('/')
-def get_all():
+def get_all() -> tuple[list[Team], int]:
     teams = storage.get_all()
     return [team.dict() for team in teams], 200
 
 
 @team_view.get('/<int:uid>')
-def get_by_id(uid):
+def get_by_id(uid: int) -> tuple[Team, int]:
     team = storage.get_by_id(uid)
     return team.dict(), 200
 
 
 @team_view.put('/<int:uid>')
-def update(uid):
+def update(uid: int) -> tuple[Team, int]:
     payload = request.json
     if not payload:
         raise AppError('empty payload')
 
-    team = Team(**payload)
+    team = TeamSchema(**payload)
     team = storage.update(team, uid)
     return team.dict(), 200
 
