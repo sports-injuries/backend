597f6b1bff5835858b8ebf729a66867b2d9135ec
alekseybeloded <belod-aleksej@yandex.ru>
2023-09-01 12:31:05 +0300

    fix: add shutdown session for db

--- backend/__main__.py
+++ backend/__main__.py
@@ -3,6 +3,7 @@ import logging
 from flask import Flask
 from pydantic import ValidationError
 
+from backend.db import db_session
 from backend.teams.errors import AppError
 from backend.teams.views import team_view
 
@@ -10,6 +11,10 @@ app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+
+def shutdown_session(exception=None):
+    db_session.remove()
 
 
 def handle_app_error(error: AppError) -> tuple[dict[str, str], int]:
@@ -22,11 +27,14 @@ def handle_validation_error(error: ValidationError) -> tuple[dict[str, str], int
 
 app.register_error_handler(AppError, handle_app_error)
 app.register_error_handler(ValidationError, handle_validation_error)
+
 app.register_blueprint(team_view, url_prefix='/api/v1/teams')
+
+app.teardown_appcontext(shutdown_session)
 
 
 def main() -> None:
-    logger.info('hello world')
+    logger.info('Run')
     app.run()
 
 
