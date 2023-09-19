b3c0ed158bf48f1cf2dab43cb4daa60fc09cf0d5
alekseybeloded <belod-aleksej@yandex.ru>
2023-09-19 20:25:13 +0300

    feat: add team_id field, name variable, ConflictError
    add team_id field for method add, get_all, get_by_id, update;
    add name variable for Storage;
    add ConflictError for method add

--- backend/players/storage.py
+++ backend/players/storage.py
@@ -1,51 +1,77 @@
+from sqlalchemy.exc import IntegrityError
+
 from backend.db import db_session
-from backend.errors import NotFoundError
+from backend.errors import ConflictError, NotFoundError
 from backend.models import Player
 from backend.players.schema import PlayerSchema
 
 
 class Storage():
+    name = 'player'
+
     def add(self, player: PlayerSchema) -> PlayerSchema:
-        entity = Player(name=player.name, description=player.description)
+        entity = Player(name=player.name, description=player.description, team_id=player.team_id)
 
-        db_session.add(entity)
-        db_session.commit()
+        try:
+            db_session.add(entity)
+            db_session.commit()
+        except IntegrityError:
+            raise ConflictError(self.name)
 
-        return PlayerSchema(uid=entity.uid, name=entity.name, description=entity.description)
+        return PlayerSchema(
+            uid=entity.uid,
+            name=entity.name,
+            description=entity.description,
+            team_id=entity.team_id,
+        )
 
     def get_all(self) -> list[PlayerSchema]:
         entities = Player.query.all()
         return [
-            PlayerSchema(uid=entity.uid, name=entity.name, description=entity.description)
-            for entity in entities
+            PlayerSchema(
+                uid=entity.uid,
+                name=entity.name,
+                description=entity.description,
+                team_id=entity.team_id,
+            ) for entity in entities
         ]
 
     def get_by_id(self, uid: int) -> PlayerSchema:
         entity = Player.query.get(uid)
 
         if not entity:
-            raise NotFoundError('player', uid)
+            raise NotFoundError(self.name, uid)
 
-        return PlayerSchema(uid=entity.uid, name=entity.name, description=entity.description)
+        return PlayerSchema(
+            uid=entity.uid,
+            name=entity.name,
+            description=entity.description,
+            team_id=entity.team_id,
+        )
 
     def update(self, player: PlayerSchema, uid: int) -> PlayerSchema:
         entity = Player.query.get(uid)
 
         if not entity:
-            raise NotFoundError('player', uid)
+            raise NotFoundError(self.name, uid)
 
         entity.name = player.name
         entity.description = player.description
 
         db_session.commit()
 
-        return PlayerSchema(uid=entity.uid, name=entity.name, description=entity.description)
+        return PlayerSchema(
+            uid=entity.uid,
+            name=entity.name,
+            description=entity.description,
+            team_id=entity.team_id,
+        )
 
     def delete(self, uid: int) -> None:
         entity = Player.query.get(uid)
 
         if not entity:
-            raise NotFoundError('player', uid)
+            raise NotFoundError(self.name, uid)
 
         db_session.delete(entity)
         db_session.commit()
