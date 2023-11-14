9576a33f31f1553aad180a2ff93debd773331955
alekseybeloded <belod-aleksej@yandex.ru>
2023-11-14 07:32:30 +0300

    fix: Change InjurySchema

--- backend/injuries/schema.py
+++ backend/injuries/schema.py
@@ -1,7 +1,11 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 
 
 class InjurySchema(BaseModel):
     uid: int
     name: str
-    description: str
+    description: str | None
+    start_date: datetime
+    end_date: datetime | None
