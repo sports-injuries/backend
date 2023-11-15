0e56cd7baf159698bd68048d10727f98d369509a
alekseybeloded <belod-aleksej@yandex.ru>
2023-11-15 19:17:22 +0300

    fix: Add correct datetime in add and get_for_player methods

--- backend/injuries/views.py
+++ backend/injuries/views.py
@@ -1,6 +1,7 @@
-from typing import Any
+from http import HTTPStatus
 
-from flask import Blueprint, request
+import orjson
+from flask import Blueprint, Response, request
 
 from backend.errors import AppError
 from backend.injuries.schema import InjurySchema
@@ -12,7 +13,7 @@ storage = Storage()
 
 
 @injury_view.post('/<int:player_id>/injuries/')
-def add(player_id: int) -> tuple[dict[str, Any], int]:
+def add(player_id: int) -> Response:
     payload = request.json
 
     if not payload:
@@ -28,16 +29,22 @@ def add(player_id: int) -> tuple[dict[str, Any], int]:
         player_id,
     )
     injury = InjurySchema.from_orm(entity)
-
-    return injury.dict(), 201
+    return Response(
+        response=orjson.dumps(injury.dict()),
+        status=HTTPStatus.CREATED,
+        content_type='application/json',
+    )
 
 
 @injury_view.get('/<int:player_id>/injuries/')
-def get_for_player(player_id: int) -> tuple[list[dict[str, Any]], int]:
+def get_for_player(player_id: int) -> Response:
     entities = storage.get_for_player(player_id)
     injuries = [InjurySchema.from_orm(entity) for entity in entities]
-
-    return [injury.dict() for injury in injuries], 200
+    return Response(
+        response=orjson.dumps([injury.dict() for injury in injuries]),
+        status=HTTPStatus.OK,
+        content_type='application/json',
+    )
 
 
 @injury_view.delete('/<int:player_id>/injuries/<int:uid>')  # type: ignore
