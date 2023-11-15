7df86ff97a4314df54b818bc6bbd6654a0d73eb4
alekseybeloded <belod-aleksej@yandex.ru>
2023-11-15 23:12:19 +0300

    feat: Add get_all_players handler, change get handler

--- backend/teams/views.py
+++ backend/teams/views.py
@@ -1,8 +1,10 @@
+from http import HTTPStatus
 from typing import Any
 
 from flask import Blueprint, request
 
 from backend.errors import AppError
+from backend.players.schema import PlayerSchema
 from backend.players.storage import Storage as PlayerStorage
 from backend.teams.schema import TeamSchema
 from backend.teams.storage import Storage as TeamStorage
@@ -32,7 +34,7 @@ def add() -> tuple[Team, int]:
 def get() -> tuple[list[Team], int]:
     name = request.args.get('name')
     if name:
-        teams = team_storage.get_by_name(name)
+        teams = team_storage.find_by_name(name)
     else:
         teams = team_storage.get_all()
 
@@ -43,6 +45,13 @@ def get() -> tuple[list[Team], int]:
 def get_by_id(uid: int) -> tuple[Team, int]:
     team = team_storage.get_by_id(uid)
     return team.dict(), 200
+
+
+@team_view.get('<int:team_id>/players/')
+def get_all_players(team_id: int) -> tuple[list[dict[str, Any]], HTTPStatus]:
+    entities = player_storage.get_for_team(team_id)
+    players = [PlayerSchema.from_orm(entity) for entity in entities]
+    return [player.dict() for player in players], HTTPStatus.OK
 
 
 @team_view.put('/<int:uid>')
